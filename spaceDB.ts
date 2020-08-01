07e023bb7ac46ed67f611a4b080b6dcc3745f066
Max Becker <max@Maxs-MacBook-Pro.local>
2020-07-31 22:50:49 -0700

    update

--- spaceDB.ts
+++ spaceDB.ts
@@ -1,6 +1,12 @@
-import feathers from '@feathersjs/feathers'
+import feathers, { Service, Id, NullableId, Params } from '@feathersjs/feathers'
 import express from '@feathersjs/express'
-import { connect, Connection } from 'rethinkdb'
+import { connect, Connection, db } from 'rethinkdb'
+
+/*
+
+this server exposes an api for the space machine to interact with the rethinkDB backend
+
+*/
 
 declare global {
   interface Window {
@@ -12,4 +18,18 @@ connect({ host: 'localhost', port: 28015 }, (err, conn) => {
   if (err) throw err
   window.rethinkDBConnection = conn
 })
+
+class SaveService implements Service<any> {
+  saveNames: string[] = []
+  async find(params: Params) { }
+  async get(id: Id, params: Params) { }
+  async create(data: any, params: Params) {
+    
+  }
+  async update(id: NullableId, data: any, params: Params) { }
+  async patch(id: NullableId, data: any, params: Params) { }
+  async remove(id: NullableId, params: Params) { }
+}
+
+
 
