1fab8b4c39e50d047be8857f130dae1601ae3bbe
Max Becker <max@Maxs-MacBook-Pro.local>
2020-07-31 20:14:55 -0700

    add lots of packages

--- spaceDB.ts
+++ spaceDB.ts
@@ -1,2 +1,15 @@
 import feathers from '@feathersjs/feathers'
+import express from '@feathersjs/express'
+import { connect, Connection } from 'rethinkdb'
+
+declare global {
+  interface Window {
+    rethinkDBConnection: Connection
+  }
+}
+
+connect({ host: 'localhost', port: 28015 }, (err, conn) => {
+  if (err) throw err
+  window.rethinkDBConnection = conn
+})
 
