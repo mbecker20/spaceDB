59dcad3b9aa72fe95a8118b04671091d3ad8a838
Max Becker <max@Maxs-MacBook-Pro.local>
2020-08-01 01:56:54 -0700

    spaceDB running

--- spaceDB.ts
+++ spaceDB.ts
@@ -1,6 +1,7 @@
 import feathers, { Id, NullableId, Params } from '@feathersjs/feathers'
 import express from '@feathersjs/express'
 import { connect, Connection, db } from 'rethinkdb'
+import cors from 'cors'
 
 /*
 
@@ -8,46 +9,40 @@ this server exposes an api for the space machine to interact with the rethinkDB 
 
 */
 
-declare global {
-  interface Window {
-    rethinkDBConnection: Connection
-  }
-}
+// find types
+const GET_SAVENAMES = 'GET_SAVENAMES'
+const GET_SAVE = 'GET_SAVE'
 
 interface Save {
   id: string,
   savedState: any,
 }
 
-connect({ host: 'localhost', port: 28015 }, (err, conn) => {
-  if (err) throw err
-  window.rethinkDBConnection = conn
-})
+let rethinkDBConnection: Connection
 
 class SaveService {
-  saveNames: string[] = []
-  async find(params?: Params) { 
-    let res: string | string[] = 'fail'
-    db('main').table('saves').pluck('id').run(window.rethinkDBConnection, (err, saveCursor) => {
-      if (err) throw err;
-      saveCursor.toArray().then(saveNames => {
-        res = saveNames
-      })
-    })
-    console.log(res)
+  async find(params?: Params) {
+    return db('main').table('saves').pluck('id').run(rethinkDBConnection)
+    .then(cursor => cursor.toArray())
+    .then(nameArray => nameArray.map(nameObj => nameObj.id))
   }
-  async get(id: Id, params: Params) { }
 
-  async create(save: Save, params?: Params) {
-    let res = 'ok!'
+  async get(id: string, params?: Params) { 
+    return db('main').table('saves').get(id).run(rethinkDBConnection)
+    .then((doc: any) => doc.state )
+  }
+
+  async create(save: Save) {
+    let res: any = 'ok!'
     db('main').table('saves').insert({
       id: save.id,
       state: save.savedState
-    }).run(window.rethinkDBConnection, err => {
+    }).run(rethinkDBConnection, (err, save) => {
       if (err) {
         res = 'not ok :('
         throw err
       }
+      res = save
     })
     return res
   }
@@ -68,19 +63,35 @@ app.use(express.urlencoded({ extended: true }));
 app.configure(express.rest());
 // Express middleware with a nicer error handler
 app.use(express.errorHandler());
+// Hopefully handles the cors error
+app.use(cors())
 
 // Register save service
 app.use('spaceDB-save-service', new SaveService());
 
-app.service('spaceDB-save-service').on('created', (save: Save) => {
-  console.log(`saved ${save.id}`)
+app.service('spaceDB-save-service').on('created', (save: any) => {
+  if (save !== 'not ok :(') {
+    console.log(`saved`)
+    console.log(save)
+  } else {
+    console.log(save)
+  }
 });
 
 app.listen(3030).on('listening', () =>
   console.log('spaceDB server listening on localhost:3030')
 );
 
-app.service('spaceDB-save-service').find()
+
+connect({ host: 'localhost', port: 28015 }, (err, conn) => {
+  if (err) {
+    console.log('fail')
+    throw err
+  }
+  rethinkDBConnection = conn
+})
+
+
 
 
 
