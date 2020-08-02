fad028c52f5b01eede0ca856e950144b169da70b
Karamvir Singh <karamvirsingh@Karamvirs-MacBook-Pro.local>
2020-08-02 16:54:17 -0700

    comir

--- setupMongoose.ts
+++ setupMongoose.ts
@@ -17,5 +17,9 @@ saveSchema.methods.getID = function() {
   const id = this.id ? this.id : 'UNDEFINED' 
 }
 
-const Save = mongoose.model('Save', saveSchema)
+id.save(function (err: {}, id: String) {
+  if (err) return console.error(err)
+  id.getID()
+}) 
 
+const Save = mongoose.model('Save', saveSchema)
