90a03acd1f01193ce824b646f0490541c6176ca8
Max Becker <max@Maxs-MacBook-Pro.local>
2020-08-02 11:32:56 -0700

    update

---
+++ configureMongo.ts
@@ -0,0 +1,7 @@
+import assert from 'assert'
+import { MongoClient } from 'mongodb'
+
+function configureMongo() {
+  // Connection URL
+  
+}
--- spaceDB.ts
+++ spaceDB.ts
@@ -1,12 +1,34 @@
 import feathers, { NullableId, Params } from '@feathersjs/feathers'
 import express from '@feathersjs/express'
 import cors from 'cors'
+import assert from 'assert'
+import { MongoClient, Db } from 'mongodb'
 
 /*
 
 this server exposes an api for the space machine to interact with the rethinkDB backend
 
 */
+
+// set up mongo connection
+// -----------------------
+
+const url = 'mongodb://localhost:27017';
+
+// Create a new MongoClient
+const client = new MongoClient(url);
+
+let db: Db
+
+// Use connect method to connect to the Server
+client.connect(function (err: any) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  db = client.db('main'); // now can use db in feather api
+
+  client.close();
+});
 
 // find types
 const GET_SAVENAMES = 'GET_SAVENAMES'
@@ -59,6 +81,8 @@ app.service('spaceDB-save-service').on('created', (save: any) => {
 app.listen(3030).on('listening', () =>
   console.log('spaceDB server listening on localhost:3030')
 );
+
+
  
 
 
