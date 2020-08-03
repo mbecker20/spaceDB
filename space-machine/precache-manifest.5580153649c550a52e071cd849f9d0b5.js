a96c6ff2a7407fefa82072bd57695b809b2b684b
Max Becker <max@Maxs-MacBook-Pro.local>
2020-08-02 23:23:05 -0700

    add space machine build

---
+++ space-machine/precache-manifest.5580153649c550a52e071cd849f9d0b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bca14389b36b9c491026f974ac100d97",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b64b954ecc4a25e317d",
+    "url": "/static/css/main.d776b7e9.chunk.css"
+  },
+  {
+    "revision": "28168c1c9a3cc24eb0db",
+    "url": "/static/js/2.9eae17e9.chunk.js"
+  },
+  {
+    "revision": "c881ea73dbcdc325602c5cb3ddd4fe98",
+    "url": "/static/js/2.9eae17e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b64b954ecc4a25e317d",
+    "url": "/static/js/main.2cd0d8d1.chunk.js"
+  },
+  {
+    "revision": "743a21509eae53ae1be1",
+    "url": "/static/js/runtime-main.4e4989bb.js"
+  },
+  {
+    "revision": "04e848f8f484417eea06b36b6874e01b",
+    "url": "/static/media/settings.04e848f8.svg"
+  }
+]);
