b31d7026430c1edd4720d690e63da43c54780cc8
Max Becker <max@Maxs-MacBook-Pro.local>
2020-08-03 00:41:18 -0700

    update

--- startServor.ts
+++ startServor.ts
@@ -5,6 +5,8 @@ const fs = require('fs')
 const options = {
   key: fs.readFileSync('../key.pem'),
   cert: fs.readFileSync('../cert.pem'),
+  requestCert: false,
+  rejectUnauthorized: false
 };
 
 function createServer() {
