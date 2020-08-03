c083a30773bdb0d419c997884ea66c7ecced5f1d
Max Becker <max@Maxs-MacBook-Pro.local>
2020-08-02 17:59:45 -0700

    update

--- spaceDB.ts
+++ spaceDB.ts
@@ -36,6 +36,7 @@ class SaveService {
     // id is savename, return the saved redux state
     return StateSaver.findOne({ saveName: saveName }, (err: any, save: any) => {
       if (err) throw err
+      console.log(save)
       return save.state
     })
   }
